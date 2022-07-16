624f99c8882e43e3a109d3e40287f3a45bb6253f
Alejandro <you@example.com>
2022-07-16 14:55:19 -0500

    This time i've finally finished this game applying the DRY principle, i hope you enjoy this little game, btw if you want to become better the game or even aply more DRY dont doubt to make a pull request, peace out ✌🏻

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 
 let score = 20;
 let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").innerHTML = message;
+};
+
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let secretNumber = (document.querySelector(".number").value = randomNumber);
 
 document.querySelector(".check").addEventListener("click", () => {
   let guessValue = Number(document.querySelector(".guess").value);
   if (guessValue === secretNumber) {
-    document.querySelector(".message").innerHTML = "You have guess!✅";
+    displayMessage("You have guess!✅");
     document.querySelector(".number").innerHTML = randomNumber;
     document.querySelector(".number").style.width = "30rem";
     document.querySelector("body").style.backgroundColor = "#60b347";
@@ -17,25 +22,16 @@ document.querySelector(".check").addEventListener("click", () => {
       document.querySelector(".highscore").innerHTML = highScore;
     }
   } else if (!guessValue) {
-    document.querySelector(".message").innerHTML = "No number 💥";
-  } else if (guessValue > secretNumber) {
+    displayMessage("No number 💥");
+  } else if (guessValue !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").innerHTML = "Too high 📈";
+      guessValue > secretNumber
+        ? displayMessage("Too high 📈")
+        : displayMessage("Too low 📉");
       score--;
       document.querySelector(".score").innerHTML = score;
     } else {
-      document.querySelector(".message").innerHTML =
-        "You have lost the game ❌";
-      document.querySelector(".score").innerHTML = 0;
-    }
-  } else if (guessValue < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").innerHTML = "Too low 📉";
-      score--;
-      document.querySelector(".score").innerHTML = score;
-    } else {
-      document.querySelector(".message").innerHTML =
-        "You have lost the game ❌";
+      displayMessage("You have lost the game ❌");
       document.querySelector(".score").innerHTML = 0;
     }
   }
@@ -46,7 +42,7 @@ document.querySelector(".again").addEventListener("click", () => {
   randomNumber = Math.trunc(Math.random() * 20) + 1;
   secretNumber = document.querySelector(".number").value = randomNumber;
   document.querySelector(".score").innerHTML = 20;
-  document.querySelector(".message").innerHTML = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").innerHTML = "?";
